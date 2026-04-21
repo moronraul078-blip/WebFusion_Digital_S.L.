3a858a2e9c4890e190fdd76039181e75e720f54c
RaulMc78 <moronraul078@gmail.com>
2026-04-21 16:45:58 +0200

    Add initial HTML structure to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>WebFusion Digital</title>
+</head>
+<body>
+    <h1>Bienvenido a WebFusion Digital S.L.</h1>
+    <p>Tu agencia de desarrollo web profesional.</p>
+</body>
+</html>
